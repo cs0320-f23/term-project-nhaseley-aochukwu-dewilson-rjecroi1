2d68a491fb3018725035f7ada1d34e7ad71891b8
nylevenya <nya_haseley-ayende@brown.edu>
2023-12-04 16:56:49 -0500

    fixed map styling for listings page

--- term-project/front/src/components/Listings.tsx
+++ term-project/front/src/components/Listings.tsx
@@ -7,108 +7,38 @@ export default function ListingsPage() {
       id: 1,
       address: "78 Brown St.",
       datePosted: "May 5, 2023",
+      url: "https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA1L2pvYjcyMC0xMTMtdi5qcGc.jpg"
     },
     {
       id: 2,
       address: "34 Brown St.",
       datePosted: "September 5, 2023",
+      url: "https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA4L2pvYjk1MC0xMDctdi5qcGc.jpg"
     },
     {
       id: 3,
       address: "46 Caliente Rd.",
       datePosted: "January 5, 2023",
+      url: "https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA1L2pvYjcxNy0wNTItdi5qcGc.jpg",
     },
   ];
 
   return (
     <div id="listings-page">
       {/* First Row */}
-      <div id="content">
+      {/* <div id="content"> */}
         <div className="row">
-          <img
-            src="https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA1L2pvYjcxNy0wNTItdi5qcGc.jpg"
-            alt="Example Listing 1"
-          />
           {mockListingInfo.map(
-            (listingImage) =>
-              listingImage.id == 3 && (
-                <div key={listingImage.id} className="listing-info">
-                  <p>Address: {listingImage.address}</p>
-                  <p>Date Posted: {listingImage.datePosted}</p>
-                </div>
-              )
+            (listingImage) => (
+              <div key={listingImage.id} className="listing-info">
+                <img src={listingImage.url} alt="Example Listing 1" />
+                <p>Address: {listingImage.address}</p>
+                <p>Date Posted: {listingImage.datePosted}</p>
+              </div>
+            )
+            // )
           )}
-        </div>
-        <div className="row">
-          <img
-            src="https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA4L2pvYjk1MC0xMDctdi5qcGc.jpg"
-            alt="Example Listing 2"
-          />
-          {mockListingInfo.map(
-            (listingImage) =>
-              listingImage.id == 2 && (
-                <div key={listingImage.id} className="listing-info">
-                  <p>Address: {listingImage.address}</p>
-                  <p>Date Posted: {listingImage.datePosted}</p>
-                </div>
-              )
-          )}
-        </div>
-        <div className="row">
-          <img
-            src="https://images.rawpixel.com/image_400/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvcGQ0My0wNjAzLTAyMy1uYW1fMS5qcGc.jpg"
-            alt="Example Listing 3"
-          />
-          {mockListingInfo.map(
-            (listingImage) =>
-              listingImage.id == 1 && (
-                <div key={listingImage.id} className="listing-info">
-                  <p>Address: {listingImage.address}</p>
-                  <p>Date Posted: {listingImage.datePosted}</p>
-                </div>
-              )
-          )}
-        </div>
-      </div>
-
-      {/* Second Row */}
-      <div id="content">
-        <div className="row">
-          <img src="https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA1L2pvYjcyMC0xMTMtdi5qcGc.jpg" />
-          {mockListingInfo.map(
-            (listingImage) =>
-              listingImage.id == 1 && (
-                <div key={listingImage.id} className="listing-info">
-                  <p>Address: {listingImage.address}</p>
-                  <p>Date Posted: {listingImage.datePosted}</p>
-                </div>
-              )
-          )}
-        </div>
-        <div className="row">
-          <img src="https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA4L2pvYjk1MC0xMDctdi5qcGc.jpg" />
-          {mockListingInfo.map(
-            (listingImage) =>
-              listingImage.id == 3 && (
-                <div key={listingImage.id} className="listing-info">
-                  <p>Address: {listingImage.address}</p>
-                  <p>Date Posted: {listingImage.datePosted}</p>
-                </div>
-              )
-          )}
-        </div>
-        <div className="row">
-          <img src="https://images.rawpixel.com/image_400/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvcGQ0My0wNjAzLTAyMy1uYW1fMS5qcGc.jpg" />
-          {mockListingInfo.map(
-            (listingImage) =>
-              listingImage.id == 2 && (
-                <div key={listingImage.id} className="listing-info">
-                  <p>Address: {listingImage.address}</p>
-                  <p>Date Posted: {listingImage.datePosted}</p>
-                </div>
-              )
-          )}
-        </div>
+        {/* </div> */}
       </div>
 
       {/* Side Control */}
