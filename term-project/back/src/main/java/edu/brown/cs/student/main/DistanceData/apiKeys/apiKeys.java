2b66e76ed4514e227d50754d19b583ae7b0383d0
d3st1n4 <destiny_wilson@brown.edu>
2023-12-15 16:19:01 -0500

    changed api keys

--- term-project/back/src/main/java/edu/brown/cs/student/main/DistanceData/apiKeys/apiKeys.java
+++ term-project/back/src/main/java/edu/brown/cs/student/main/DistanceData/apiKeys/apiKeys.java
@@ -2,6 +2,6 @@ package edu.brown.cs.student.main.DistanceData.apiKeys;
 
 public class apiKeys {
 
-    public static final String DISTANCE_API_KEY = "7ozzBXCWyNlHauEGeQVXMEIJZb5w1QGGrdFxn04M8hZHwXD1CF3aTmgX8jDmOq1Y";
-    public static final String CONVERSION_API_KEY = "dmT2JQUecArgbbpgg8EFqvT29ktu7T1TIUFCUtUsc2tWkLwB8ge3Hs6kr3SsKRyM";
+    public static final String DISTANCE_API_KEY = "te00uxvsGtEjJqhIofiEgsgf8Rv63VXDHQsFomWClGeL56wu8AZAC07m8qrG6Qgv";
+    public static final String CONVERSION_API_KEY = "zB3oFcXJlEi9g5QHR1n2CYWm3PwXnju2YtlXfyyvO91oH8M3MSSRmME5Sd5Mh1gw";
 }
