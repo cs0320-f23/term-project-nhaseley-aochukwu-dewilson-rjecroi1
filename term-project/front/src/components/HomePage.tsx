291ee4f1fa7df12e659d4bd0340d48ff94dd7147
d3st1n4 <destiny_wilson@brown.edu>
2023-12-17 18:03:26 -0500

    feat: added styling for admin page

--- term-project/front/src/components/HomePage.tsx
+++ term-project/front/src/components/HomePage.tsx
@@ -2,7 +2,7 @@ import "../styles/Homepage.css";
 
 /**
  * HomePage Component
- * 
+ *
  * This component represents the landing page of the application.
  * It provides an overview of the mission of the housing locator application
  * and encourages users to start exploring housing opportunities.
@@ -10,13 +10,12 @@ import "../styles/Homepage.css";
 export default function HomePage() {
   return (
     <div className="homepage">
-      <h1>Brown Intern Summer Housing - Boston</h1>—————<h2> Mission</h2>
+      <h1>Brown Intern Summer Housing - Boston</h1>———<h2> Mission</h2>
       <p className="homepage-description">
-        BISH Boston is a housing
-        locator application made for students at Brown University
-        seeking summer intern housing in the greater Boston area. Now, finding
-        housing is as easy as a click of a button. Register below to see
-        what housing opportunities await!
+        BISH Boston is a housing locator application made for students at Brown
+        University seeking summer intern housing in the greater Boston area.
+        Now, finding housing is as easy as a click of a button. Register below
+        to see what housing opportunities await!
       </p>
       <a href="/register">
         <button type="start">START</button>
